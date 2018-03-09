800866bcd502051f53bb1ae81b5b10b333a0806d
Ajmal Manzoor <ajmal_manzoor@hotmail.co.uk>
2018-03-09 18:00:22 +0000

    Uploaded news scraper tag recommender

---
+++ TagGenerator/NewsHeadlinesScraper.py
@@ -0,0 +1,35 @@
+import selenium.webdriver as webdriver
+import time
+import os
+
+def get_results(search_term):
+    url = "https://news.google.com/news/?ned=uk&gl=GB&hl=en-GB"
+    browser = webdriver.Firefox()
+    browser.get(url)
+    
+    search_box = browser.find_element_by_class_name("Ax4B8.ZAGvjd")
+    search_box.send_keys(search_term)
+    search_box.send_keys(u'\ue007')
+    search_box.submit()
+    new_url = browser.current_url
+    print(new_url)
+    browser.close()
+    os.system("taskkill /im geckodriver.exe")
+
+    browser = webdriver.Firefox()
+    browser.get(new_url)
+    time.sleep(2)
+    news_items = browser.find_elements_by_class_name("nuEeue.hzdq5d.ME7ew")
+    for news_item in news_items:
+        print(news_item.text)
+
+    browser.close()
+    os.system("taskkill /im geckodriver.exe")
+    
+
+
+get_results("cat")
+
+
+
+
