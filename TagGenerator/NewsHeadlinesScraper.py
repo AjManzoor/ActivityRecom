7badfe6baa5231d50141d618b79b5b9c356682ad
Ajmal Manzoor <ajmal_manzoor@hotmail.co.uk>
2018-03-12 19:30:52 +0000

    Added neo4j to python script

--- TagGenerator/NewsHeadlinesScraper.py
+++ TagGenerator/NewsHeadlinesScraper.py
@@ -1,6 +1,17 @@
 import selenium.webdriver as webdriver
 import time
 import os
+from neo4j.v1 import GraphDatabase
+
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+
+def add_to_graph_db(noun):
+    with driver.session() as session:
+        with session.begin_transaction() as tx:
+            tx.run("CREATE(a:Person {name:{noun}}) RETURN a", noun= noun)
+        
+    
 
 def get_results(search_term):
     url = "https://news.google.com/news/?ned=uk&gl=GB&hl=en-GB"
